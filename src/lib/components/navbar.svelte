759b8217459f1792f49561f5b54957aec097b88b
Ross Keenan <rossk29@gmail.com>
2023-01-31 22:16:12 +0200

    CSS only responsiveness

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,54 +1,34 @@
-<script lang=ts>
-	import Breakpoints from 'svelte-breakpoints';
-	import type { BreakpointMatch } from 'svelte-breakpoints';
-
-	const mediaQueries = {
-		sm: '(min-width: 0px)',
-    	md: '(min-width: 480px)',
-	};
-
-	let match: BreakpointMatch;
+<script lang="ts">
+	const routes: { href: string; label: string }[] = [
+		{ href: '/', label: 'Home' },
+		{ href: '#portfolio', label: 'Portfolio' },
+		{ href: '#services', label: 'Services' },
+		{ href: '#contact-us', label: 'Contact Us' }
+	];
 </script>
 
 <nav class="navbar justify-center">
-
-	<Breakpoints queries={mediaQueries} bind:match>
-		{#if $match === 'sm'}
-			<div class="dropdown dropdown-bottom">
-				<label tabindex="0" class="btn m-1">Menu</label>
-				<ul class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-					<!-- TODO: refactor this navigation list -->
+	<div class="sm:hidden block">
+		<!-- Mobile -->
+		<div class="dropdown dropdown-bottom">
+			<label tabindex="0" class="btn m-1">Menu</label>
+			<ul class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+				{#each routes as { href, label }}
 					<li>
-						<a href="/" on:click={() => window.scrollTo(0, 0)}> Home </a>
+						<a {href}>{label}</a>
 					</li>
-					<li>
-						<a href="#portfolio"> Portfolio </a>
-					</li>
-					<li>
-						<a href="#services"> Services </a>
-					</li>
-			
-					<li>
-						<a href="#contact-us"> Contact Us</a>
-					</li>
-				</ul>
-			</div>
-		{:else}
-			<ul class="py-10 flex gap-10 text-xl">
-				<li>
-					<a class="hover:underline" href="/" on:click={() => window.scrollTo(0, 0)}> Home </a>
-				</li>
-				<li>
-					<a class="hover:underline" href="#portfolio"> Portfolio </a>
-				</li>
-				<li>
-					<a class="hover:underline" href="#services"> Services </a>
-				</li>
-		
-				<li>
-					<a class="hover:underline" href="#contact-us"> Contact Us</a>
-				</li>
+				{/each}
 			</ul>
-		{/if}
-	</Breakpoints>	
+		</div>
+	</div>
+	<div class="sm:block hidden">
+		<!-- Desktop -->
+		<ul class="py-10 flex gap-10 text-xl">
+			{#each routes as { href, label }}
+				<li>
+					<a {href}>{label}</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
 </nav>
