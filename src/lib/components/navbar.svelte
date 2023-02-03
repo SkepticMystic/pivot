18cc7f0c7a926a64e4dbb0979ab889c5031300cf
Ross Keenan <rossk29@gmail.com>
2023-02-03 15:58:23 +0200

    Cleaner navbar menu on mobile

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Bars3 from './icons/bars3.svelte';
+
 	const routes: { href: string; label: string }[] = [
 		{ href: '/', label: 'Home' },
 		{ href: '#portfolio', label: 'Portfolio' },
@@ -7,15 +9,16 @@
 	];
 </script>
 
-<nav class="navbar justify-center">
+<nav class="navbar sm:justify-center p-4">
 	<div class="sm:hidden block">
 		<!-- Mobile -->
-		<div class="dropdown dropdown-bottom">
-			<label tabindex="0" class="btn m-1">Menu</label>
+		<div class="dropdown dropdown-bottom dropdown-hover">
+			<!-- svelte-ignore a11y-label-has-associated-control -->
+			<label class="btn btn-ghost btn-sqaure"><Bars3 /></label>
 			<ul class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
 				{#each routes as { href, label }}
 					<li>
-						<a {href}>{label}</a>
+						<a {href}><h4>{label}</h4></a>
 					</li>
 				{/each}
 			</ul>
@@ -23,10 +26,10 @@
 	</div>
 	<div class="sm:block hidden">
 		<!-- Desktop -->
-		<ul class="py-10 flex gap-10 text-xl">
+		<ul class="py-10 flex gap-12 text-xl">
 			{#each routes as { href, label }}
 				<li>
-					<a {href}>{label}</a>
+					<a {href}><h5>{label}</h5></a>
 				</li>
 			{/each}
 		</ul>
