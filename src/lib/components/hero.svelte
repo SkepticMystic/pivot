ba4dbc050231df4936abb46909b456a93676b991
Ross Keenan <rossk29@gmail.com>
2023-02-04 14:37:27 +0200

    Hero

---
+++ src/lib/components/hero.svelte
@@ -0,0 +1,22 @@
+<!-- This example requires Tailwind CSS v3.0+ -->
+<div class="isolate">
+	<main>
+		<div class="relative">
+			<div class="mx-auto max-w-2xl pt-12 pb-12 sm:pt-16 lg:pt-32 lg:pb-20">
+				<div class="text-center">
+					<h1 class="">Bespoke Web Development</h1>
+					<p class="mt-5 text-xl text-gray-500">
+						We build bespoke web applications for your business. We can help you with your website,
+						web application, or mobile application.
+					</p>
+					<div class="mt-10 flex sm:flex-row flex-col items-center justify-center gap-x-6 gap-y-4">
+						<a href="#contact-us" class="btn btn-primary"> Contact Us </a>
+						<a href="#team" class="text-base font-semibold leading-7 text-gray-900"
+							>Learn more <span aria-hidden="true">→</span></a
+						>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+</div>
