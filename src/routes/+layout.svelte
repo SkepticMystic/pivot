76cffd0d0b1018aa7e58c45836c29b742889b76f
Ross Keenan <rossk29@gmail.com>
2023-02-04 14:37:35 +0200

    Hide scrollToTop at top

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import ArrowSmallUp from '$lib/components/icons/arrowSmallUp.svelte';
 	import Navbar from '$lib/components/navbar.svelte';
+	import { fly } from 'svelte/transition';
 	import '../app.css';
+
+	let y: number;
 </script>
 
+<svelte:window bind:scrollY={y} />
 <svelte:head>
 	<title>SeaShell | Web Development South Africa</title>
 </svelte:head>
@@ -11,14 +15,18 @@
 <div class="bg-[#f3ebe1] font-['Roboto']">
 	<Navbar />
 
-	<main class="px-[10%] lg:py-10 py-2">
+	<main class="px-[8%]">
 		<slot />
 	</main>
 
-	<button
-		class="btn btn-primary rounded-full btn-square fixed bottom-5 right-5 md:bottom-8 md:right-8 hover:scale-110"
-		on:click={() => window.scrollTo(0, 0)}
-	>
-		<ArrowSmallUp />
-	</button>
+	{#if y > 100}
+		<button
+			class="btn btn-primary rounded-full btn-square fixed bottom-5 right-5 md:bottom-8 md:right-8 hover:scale-110"
+			title="Back to top"
+			transition:fly={{ duration: 400 }}
+			on:click={() => window.scrollTo(0, 0)}
+		>
+			<ArrowSmallUp />
+		</button>
+	{/if}
 </div>
