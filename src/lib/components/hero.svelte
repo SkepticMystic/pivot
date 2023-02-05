89f8af464aa66fabaeb606bbdfe4b1b0eceac78b
Ross Keenan <rossk29@gmail.com>
2023-02-05 09:26:11 +0200

    Update hero copy

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -6,7 +6,8 @@
 				<div class="text-center">
 					<h1 class="">Pivot Web Development</h1>
 					<p class="mt-5 text-xl text-gray-600">
-						We build bespoke web applications for your business.
+						We build bespoke web applications for your business. We are a team of developers with a
+						passion for building.
 					</p>
 					<div class="mt-10 flex sm:flex-row flex-col items-center justify-center gap-x-6 gap-y-4">
 						<a href="#contact-us" class="btn btn-primary"> Contact Us </a>
