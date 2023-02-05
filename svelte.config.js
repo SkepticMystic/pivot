43022e3e39fd0671972c432b551bbc7486994bf4
Ross Keenan <rossk29@gmail.com>
2023-02-05 17:02:46 +0200

    Add GTM

--- svelte.config.js
+++ svelte.config.js
@@ -15,8 +15,26 @@ const config = {
 		adapter: adapter(),
 		csp: {
 			directives: {
-				'script-src': ["'self'", 'vitals.vercel-insights.com', 'https://cdn.vercel-insights.com'],
-				'connect-src': ["'self'", 'https://vitals.vercel-analytics.com/']
+				'script-src': [
+					"'self'",
+					'vitals.vercel-insights.com',
+					'https://cdn.vercel-insights.com',
+					'https://*.googletagmanager.com',
+					'https://www.googletagmanager.com/'
+				],
+				'connect-src': [
+					"'self'",
+					'https://vitals.vercel-analytics.com/',
+					'https://*.google-analytics.com',
+					'https://*.analytics.google.com',
+					'https://*.googletagmanager.com'
+				],
+				'img-src': [
+					"'self'",
+					'data:',
+					'https://*.google-analytics.com',
+					'https://*.googletagmanager.com'
+				]
 			}
 		}
 	}
