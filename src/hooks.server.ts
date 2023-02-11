71e722433565ed510a9707d64ddd57ccaf8359f3
Ross Keenan <rossk29@gmail.com>
2023-02-11 14:43:08 +0200

    fix: await mongoose connection

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 try {
     mongoose.set("strictQuery", true);
-    mongoose.connect(MONGO_URI, { autoIndex: false, dbName: 'pivot' });
+    await mongoose.connect(MONGO_URI, { autoIndex: false, dbName: 'pivot' });
     console.log("Connected to MongoDB");
 } catch (error) {
     console.log(error);
